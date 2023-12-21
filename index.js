4155b015765158be9fc622e4b5633a3d1328ea66
Rikesh Shrestha <rikeshmania123@gmail.com>
2023-12-21 16:17:01 +0545

    ui changed and api issue fix also workked on the chart

--- index.js
+++ index.js
@@ -1,105 +1,98 @@
-const userInput = document.getElementById("userInput").value;
+let options = {
+  chart: {
+      height: 380,
+      type: "line",
+      foreColor: '#fff'
+  },
+  series: [
+      {
+          name: "Temperature",
+          data: [15, 20, 25, 29, 33, 36, 37, 32, 27, 23]
+      }
+  ],
+  fill: {
+      type: "gradient",
+      gradient: {
+          type: 'vertical',
+          shadeIntensity: 1,
+          opacityFrom: 1,
+          opacityTo: 1,
+          colorStops: [
+              {
+                  offset: 10,
+                  color: "#fc440b",
+                  opacity: 1
+              },
+              {
+                  offset: 55,
+                  color: "#ffce63",
+                  opacity: 1
+              },
+              {
+                  offset: 90,
+                  color: "#0a95f9",
+                  opacity: 1
+              }
+          ]
+      }
+  },
+  grid: {
+      borderColor: '#6D6D6D'
+  },
+  stroke: {
+      curve: 'smooth'
+  },
+  yaxis: {
+      min: 0,
+      max: 45
+  },
+  xaxis: {
+      type: 'category',
+      tickAmount: 8,
+      categories: [
+          ['4 AM'],
+          ['6 AM'],
+          ['8 AM'],
+          ['10 AM'],
+          ['12 PM'],
+          ['2 PM'],
+          ['4 PM'],
+          ['6 PM'],
+          ['8 PM'],
+          ['10 PM']
+      ],
+      labels: {
+          show: true
+      }
+  }
+};
 
-      
-        var options = {
-          series: [
-          {
-            name: "High - 2023",
-            data: [28, 29, 33, 36, 32, 32, 33]
-          },
-          {
-            name: "Low - 2023",
-            data: [12, 11, 14, 18, 17, 13, 13],
-          }
-        ],
-          chart: {
-          height: 350,
-          type: 'line',
-          dropShadow: {
-            enabled: true,
-            color: 'white',
-            top: 18,
-            left: 7,
-            blur: 10,
-            opacity: 0.2
-          },
-          toolbar: {
-            show: false
-          }
-        },
-        
-        dataLabels: {
-          enabled: true,
-        },
-        colors: ['#77B6EA', '#545454'],
-        dataLabels: {
-          enabled: true,
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        title: {
-          text: 'Average High & Low Temperature',
-          align: 'left'
-        },
-        grid: {
-          borderColor: '#e7e7e7',
-          row: {
-            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-            opacity: 0.5
-          },
-        },
+let chart = new ApexCharts(document.querySelector("#chart"), options);
 
-        markers: {
-          size: 1
-        },
-        xaxis: {
-          categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-         
-          title: {
-            text: `Temperature this week of ${userInput}`,
-          }
-        },
-        yaxis: {
-          title: {
-            text: 'Temperature'
-          },
-          min: 5,
-          max: 40
-        },
-        legend: {
-          position: 'top',
-          horizontalAlign: 'right',
-          floating: true,
-          offsetY: -25,
-          offsetX: -5,
-        }
-        };
+chart.render();
 
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
-
-        let weather__date  = document.getElementById('weather__date')
-        weather__date.textContent = new Date()
+let weather__date = document.getElementById('weather__date')
+weather__date.textContent = new Date()
 async function fetchWeather(event) {
   event.preventDefault();
+  const userInput = document.getElementById("userInput").value;
   const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${userInput}`;
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "29b07914aemshbbb733b3a9594e2p15a0c0jsna427e95f6301",
-      "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
-    },
+      'X-RapidAPI-Key': '29b07914aemshbbb733b3a9594e2p15a0c0jsna427e95f6301',
+      'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
   };
 
   try {
-
     const response = await fetch(url, options);
     const result = await response.json();
     console.log(result);
-    if(result.temp !== undefined && result.humidity !== undefined){
+    if (result.temp !== undefined && result.humidity !== undefined) {
+     
       let temperature = document.getElementById('weather__detail__temperature');
-      temperature.textContent =`${result.temp}°C`;
+      temperature.textContent = `${result.temp}°C`;
       let city = document.getElementById('city');
       city.textContent = userInput
       let wind = document.getElementById('wind');
@@ -115,15 +108,13 @@ async function fetchWeather(event) {
       let sunsetUnix = new Date(result.sunset * 1000)
       Sunset.textContent = sunsetUnix.toLocaleTimeString();
       let max_temper = document.getElementById('maxtemperature');
-      max_temper.textContent = `${result.max_temp+ 1}°C`;
+      max_temper.textContent = `${result.max_temp + 1}°C`;
       let min_temper = document.getElementById('mintemperature');
-      min_temper.textContent = `${result.min_temp }°C`;
-    }else{
-      alert('Please enter a correct city name!')
+      min_temper.textContent = `${result.min_temp}°C`;
+    } else {
+     alert('Please enter a correct city name')
     }
- 
   } catch (error) {
-    console.error(error);
-    userInput.textContent =""
+    console(error);
   }
 }
